06d49e8a17c03b52ea05fb8dde2695f635e059d8
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-04 17:50:00 +0200

    Etiquetas a cada index de sección

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,8 +39,10 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Aeropuertos', 'url' => ['/aeropuertos/index']],
+            ['label' => 'Compañías', 'url' => ['/companias/index']],
+            ['label' => 'Vuelos', 'url' => ['/vuelos/index']],
+            ['label' => 'Reservas', 'url' => ['/reservas/index']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
