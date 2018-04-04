b79b442410bdaeffc779183c83a1e247acb17a4f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-04 19:08:24 +0200

    Implemento Usuario

--- models/Usuarios.php
+++ models/Usuarios.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "usuarios".
@@ -13,7 +14,7 @@ use Yii;
  *
  * @property Reservas[] $reservas
  */
-class Usuarios extends \yii\db\ActiveRecord
+class Usuarios extends \yii\db\ActiveRecord implements IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -53,5 +54,61 @@ class Usuarios extends \yii\db\ActiveRecord
     public function getReservas()
     {
         return $this->hasMany(Reservas::className(), ['usuario_id' => 'id'])->inverseOf('usuario');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        foreach (self::$users as $user) {
+            if ($user['accessToken'] === $token) {
+                return new static($user);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+    }
+
+    /**
+     * Este método valida la contraseña comparándola con la codificada en la BD.
+     *
+     * @param string $password La contraseña que se validará
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword(
+            $password, $this->password
+        );
     }
 }
